e5a58c7cefe48971d747de9dea99b43f8519f40d
jansich <jansich@users.noreply.github.com>
2020-02-14 21:34:30 +0100

    Update address_book_process.php

--- catalog/includes/languages/czech/address_book_process.php
+++ catalog/includes/languages/czech/address_book_process.php
@@ -10,30 +10,30 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE_1', 'My Account');
-define('NAVBAR_TITLE_2', 'Address Book');
+define('NAVBAR_TITLE_1', 'Můj účet');
+define('NAVBAR_TITLE_2', 'Adresář');
 
-define('NAVBAR_TITLE_ADD_ENTRY', 'New Entry');
-define('NAVBAR_TITLE_MODIFY_ENTRY', 'Update Entry');
-define('NAVBAR_TITLE_DELETE_ENTRY', 'Delete Entry');
+define('NAVBAR_TITLE_ADD_ENTRY', 'Přidat adresu');
+define('NAVBAR_TITLE_MODIFY_ENTRY', 'Upravit adresu');
+define('NAVBAR_TITLE_DELETE_ENTRY', 'Smazat adresu');
 
-define('HEADING_TITLE_ADD_ENTRY', 'New Address Book Entry');
-define('HEADING_TITLE_MODIFY_ENTRY', 'Update Address Book Entry');
-define('HEADING_TITLE_DELETE_ENTRY', 'Delete Address Book Entry');
+define('HEADING_TITLE_ADD_ENTRY', 'Přidat adresu');
+define('HEADING_TITLE_MODIFY_ENTRY', 'Upravit adresu');
+define('HEADING_TITLE_DELETE_ENTRY', 'Smazat adresu');
 
-define('DELETE_ADDRESS_TITLE', 'Delete Address');
-define('DELETE_ADDRESS_DESCRIPTION', 'Are you sure you would like to delete the selected address from your address book?');
+define('DELETE_ADDRESS_TITLE', 'Smazat adresu');
+define('DELETE_ADDRESS_DESCRIPTION', 'Jste si jisti, že chcete tuto adresu odstranit z Vašeho adresáře?');
 
-define('NEW_ADDRESS_TITLE', 'New Address Book Entry');
+define('NEW_ADDRESS_TITLE', 'Přidání adresy');
 
-define('SELECTED_ADDRESS', 'Selected Address');
-define('SET_AS_PRIMARY', 'Set as primary address.');
+define('SELECTED_ADDRESS', 'Vybraná adresa');
+define('SET_AS_PRIMARY', 'Nastavit jako výchozí.');
 
-define('SUCCESS_ADDRESS_BOOK_ENTRY_DELETED', 'The selected address has been successfully removed from your address book.');
-define('SUCCESS_ADDRESS_BOOK_ENTRY_UPDATED', 'Your address book has been successfully updated.');
+define('SUCCESS_ADDRESS_BOOK_ENTRY_DELETED', 'Vybraná adresa byla vymazána z Vašeho adresáře.');
+define('SUCCESS_ADDRESS_BOOK_ENTRY_UPDATED', 'Změny byly uloženy.');
 
-define('WARNING_PRIMARY_ADDRESS_DELETION', 'The primary address cannot be deleted. Please set another address as the primary address and try again.');
+define('WARNING_PRIMARY_ADDRESS_DELETION', 'Výchozí adresa nemůže být smazána. Změňte výchozí adresu a zkuste to znovu.');
 
-define('ERROR_NONEXISTING_ADDRESS_BOOK_ENTRY', 'The address book entry does not exist.');
-define('ERROR_ADDRESS_BOOK_FULL', 'Your address book is full. Please delete an unneeded address to save a new one.');
+define('ERROR_NONEXISTING_ADDRESS_BOOK_ENTRY', 'Adresa v adresáři neexistuje.');
+define('ERROR_ADDRESS_BOOK_FULL', 'Váš adresář je plný. Smažte nepotřebné adresy před přidáním nových.');
 ?>
