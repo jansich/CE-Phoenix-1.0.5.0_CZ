daef56626ec5604c046508c8c93dad745ea7f5ec
jansich <jansich@users.noreply.github.com>
2020-02-14 20:58:06 +0100

    Update account_newsletters.php

--- catalog/includes/languages/czech/account_newsletters.php
+++ catalog/includes/languages/czech/account_newsletters.php
@@ -10,13 +10,13 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE_1', 'My Account');
-define('NAVBAR_TITLE_2', 'Newsletter Subscriptions');
+define('NAVBAR_TITLE_1', 'Můj účet');
+define('NAVBAR_TITLE_2', 'Nastavení zasílání informací o novinkách');
 
-define('HEADING_TITLE', 'Newsletter Subscriptions');
+define('HEADING_TITLE', 'Nastavení zasílání informací o novinkách');
 
-define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'General Newsletter');
-define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Including store news, new products, special offers, and other promotional announcements.');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Newsletter');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Zprávy o našem obchodu, nových produktech, zvláštních nabídkách a další oznámení.');
 
-define('SUCCESS_NEWSLETTER_UPDATED', 'Your newsletter subscriptions have been successfully updated.');
+define('SUCCESS_NEWSLETTER_UPDATED', 'Nastavení bylo upraveno.');
 ?>
