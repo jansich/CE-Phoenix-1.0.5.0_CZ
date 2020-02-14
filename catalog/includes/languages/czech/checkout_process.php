89a2e276394b5ac609bc2fc3fbd507f56448caff
jansich <jansich@users.noreply.github.com>
2020-02-14 23:38:41 +0100

    Update checkout_process.php

--- catalog/includes/languages/czech/checkout_process.php
+++ catalog/includes/languages/czech/checkout_process.php
@@ -10,18 +10,18 @@
   Released under the GNU General Public License
 */
 
-define('EMAIL_TEXT_SUBJECT', 'Order Process');
-define('EMAIL_TEXT_ORDER_NUMBER', 'Order Number:');
-define('EMAIL_TEXT_INVOICE_URL', 'Detailed Invoice:');
-define('EMAIL_TEXT_DATE_ORDERED', 'Date Ordered:');
-define('EMAIL_TEXT_PRODUCTS', 'Products');
-define('EMAIL_TEXT_SUBTOTAL', 'Sub-Total:');
-define('EMAIL_TEXT_TAX', 'Tax:        ');
-define('EMAIL_TEXT_SHIPPING', 'Shipping: ');
-define('EMAIL_TEXT_TOTAL', 'Total:    ');
-define('EMAIL_TEXT_DELIVERY_ADDRESS', 'Delivery Address');
-define('EMAIL_TEXT_BILLING_ADDRESS', 'Billing Address');
-define('EMAIL_TEXT_PAYMENT_METHOD', 'Payment Method');
+define('EMAIL_TEXT_SUBJECT', 'Informace o objednávce');
+define('EMAIL_TEXT_ORDER_NUMBER', 'Číslo objednávky:');
+define('EMAIL_TEXT_INVOICE_URL', 'Detail objednávky:');
+define('EMAIL_TEXT_DATE_ORDERED', 'Datum objednání:');
+define('EMAIL_TEXT_PRODUCTS', 'Produkty');
+define('EMAIL_TEXT_SUBTOTAL', 'Mezisoučet:');
+define('EMAIL_TEXT_TAX', 'Daň:        ');
+define('EMAIL_TEXT_SHIPPING', 'Doprava: ');
+define('EMAIL_TEXT_TOTAL', 'Celkem:    ');
+define('EMAIL_TEXT_DELIVERY_ADDRESS', 'Doručovací adresa');
+define('EMAIL_TEXT_BILLING_ADDRESS', 'Fakturační adresa');
+define('EMAIL_TEXT_PAYMENT_METHOD', 'Způsob platby');
 
 define('EMAIL_SEPARATOR', '------------------------------------------------------');
 define('TEXT_EMAIL_VIA', 'via');
