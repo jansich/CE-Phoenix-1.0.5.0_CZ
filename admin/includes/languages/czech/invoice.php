9cde766236a2eebe88f4d2a2779bacead29209a1
jansich <jansich@users.noreply.github.com>
2020-02-14 18:32:19 +0100

    Add files via upload

---
+++ admin/includes/languages/czech/invoice.php
@@ -0,0 +1,34 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('TABLE_HEADING_COMMENTS', 'Comments');
+
+define('TABLE_HEADING_QTY', 'Qty');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_TAX', 'Tax');
+define('TABLE_HEADING_TOTAL', 'Total');
+define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Price (ex)');
+define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Price (inc)');
+define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Total (ex)');
+define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Total (inc)');
+
+define('ENTRY_SOLD_TO', 'SOLD TO:');
+define('ENTRY_SHIP_TO', 'SHIP TO:');
+define('ENTRY_PAYMENT_METHOD', 'Payment Method: %s');
+define('ENTRY_SUB_TOTAL', 'Sub-Total:');
+define('ENTRY_TAX', 'Tax:');
+define('ENTRY_SHIPPING', 'Shipping:');
+define('ENTRY_TOTAL', 'Total:');
+
+define('ENTRY_INVOICE_NUMBER', 'Invoice: %s');
+define('ENTRY_INVOICE_DATE', 'Date: %s');
---
+++ admin/includes/languages/czech/packingslip.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('TABLE_HEADING_COMMENTS', 'Comments');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_QTY', 'Qty');
+
+define('ENTRY_SOLD_TO', 'SOLD TO:');
+define('ENTRY_SHIP_TO', 'SHIP TO:');
+define('ENTRY_PAYMENT_METHOD', 'Payment Method: %s');
+
+define('ENTRY_INVOICE_NUMBER', 'Invoice: %s');
+define('ENTRY_INVOICE_DATE', 'Date: %s');
