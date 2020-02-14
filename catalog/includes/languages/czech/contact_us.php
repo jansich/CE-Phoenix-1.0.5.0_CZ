8e475ee8e8f076e4216533d335a178605aa43733
jansich <jansich@users.noreply.github.com>
2020-02-15 00:15:52 +0100

    Update contact_us.php

--- catalog/includes/languages/czech/contact_us.php
+++ catalog/includes/languages/czech/contact_us.php
@@ -10,16 +10,16 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Contact Us');
-define('NAVBAR_TITLE', 'Contact Us');
-define('TEXT_SUCCESS', 'Your enquiry has been successfully sent to the Store Owner.');
-define('EMAIL_SUBJECT', 'Enquiry from %s');
+define('HEADING_TITLE', 'Kontaktujte nás');
+define('NAVBAR_TITLE', 'Kontaktujte nás');
+define('TEXT_SUCCESS', 'Váš dotaz byl odeslán.');
+define('EMAIL_SUBJECT', 'Dotaz z obchodu ' . STORE_NAME);
 
-define('ENTRY_NAME', 'Full Name');
+define('ENTRY_NAME', 'Vaše jméno');
 define('ENTRY_NAME_TEXT', '');
-define('ENTRY_EMAIL', 'E-Mail Address');
+define('ENTRY_EMAIL', 'Váš e-mail');
 define('ENTRY_EMAIL_TEXT', '');
-define('ENTRY_ENQUIRY', 'Enquiry');
+define('ENTRY_ENQUIRY', 'Dotaz');
 define('ENTRY_ENQUIRY_TEXT', '');
 
-define('ERROR_ACTION_RECORDER', 'Error: An enquiry has already been sent. Please try again in %s minutes.');
+define('ERROR_ACTION_RECORDER', 'Chyba: Ochrana proti spamu - bylo odesláno mnoho dotazů. Prosím zkuste to znovu za %s minut.');
