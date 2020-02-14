6ef9ded16dac919eea4962d8d1206892fbe45236
jansich <jansich@users.noreply.github.com>
2020-02-14 20:52:08 +0100

    Update account_history_info.php

--- catalog/includes/languages/czech/account_history_info.php
+++ catalog/includes/languages/czech/account_history_info.php
@@ -10,32 +10,32 @@
   Released under the GNU General Public License
 */
 
-define('NAVBAR_TITLE_1', 'My Account');
-define('NAVBAR_TITLE_2', 'History');
-define('NAVBAR_TITLE_3', 'Order #%s');
+define('NAVBAR_TITLE_1', 'Můj účet');
+define('NAVBAR_TITLE_2', 'Historie objednávek');
+define('NAVBAR_TITLE_3', 'Objednávka číslo #%s');
 
-define('HEADING_TITLE', 'Order Information');
+define('HEADING_TITLE', 'Informace o objednávce');
 
-define('HEADING_ORDER_NUMBER', 'Order #%s');
-define('HEADING_ORDER_DATE', 'Order Date:');
-define('HEADING_ORDER_TOTAL', 'Order Total:');
+define('HEADING_ORDER_NUMBER', 'Objednávka číslo #%s');
+define('HEADING_ORDER_DATE', 'Datum objednání:');
+define('HEADING_ORDER_TOTAL', 'Cena s DPH:');
 
-define('HEADING_DELIVERY_ADDRESS', 'Delivery Address');
-define('HEADING_SHIPPING_METHOD', 'Shipping Method');
+define('HEADING_DELIVERY_ADDRESS', 'Doručovací adresa');
+define('HEADING_SHIPPING_METHOD', 'Způsob doručení');
 
-define('HEADING_PRODUCTS', 'Products');
-define('HEADING_TAX', 'Tax');
-define('HEADING_TOTAL', 'Total');
+define('HEADING_PRODUCTS', 'Produkty');
+define('HEADING_TAX', 'Daň');
+define('HEADING_TOTAL', 'Celkem s DPH');
 
-define('HEADING_BILLING_INFORMATION', 'Billing Information');
-define('HEADING_BILLING_ADDRESS', 'Billing Address');
-define('HEADING_PAYMENT_METHOD', 'Payment Method');
+define('HEADING_BILLING_INFORMATION', 'Fakturační údaje');
+define('HEADING_BILLING_ADDRESS', 'Fakturační adresa');
+define('HEADING_PAYMENT_METHOD', 'Způsob platby');
 
-define('HEADING_ORDER_HISTORY', 'Order History');
-define('HEADING_COMMENT', 'Comments');
-define('TEXT_NO_COMMENTS_AVAILABLE', 'No comments available.');
+define('HEADING_ORDER_HISTORY', 'Stav objednávky');
+define('HEADING_COMMENT', 'Poznámky');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'Žádné poznámky.');
 
-define('TABLE_HEADING_DOWNLOAD_DATE', 'Link expires: ');
-define('TABLE_HEADING_DOWNLOAD_COUNT', ' downloads remaining');
-define('HEADING_DOWNLOAD', 'Download links');
+define('TABLE_HEADING_DOWNLOAD_DATE', 'Platnost odkazu do: ');
+define('TABLE_HEADING_DOWNLOAD_COUNT', ' stažení zbývá');
+define('HEADING_DOWNLOAD', 'Odkazy pro stažení');
 ?>
