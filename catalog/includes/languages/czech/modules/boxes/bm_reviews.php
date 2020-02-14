3317c1017cebe9675850ecfa4a851504b2b351e5
jansich <jansich@users.noreply.github.com>
2020-02-14 18:45:01 +0100

    Add files via upload

---
+++ catalog/includes/languages/czech/modules/boxes/bm_reviews.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_BOXES_REVIEWS_TITLE', 'Reviews');
+  define('MODULE_BOXES_REVIEWS_DESCRIPTION', 'Show product reviews');
+  
+  define('MODULE_BOXES_REVIEWS_BOX_TITLE', 'Reviews');
+  
